f4723182ed39e7ee26390f4d1954a336e7108e21
Rakshit Sakhuja <masterkidster@gmail.com>
2021-02-28 14:07:26 +0530

    Revert "changed type() to options() in all the C code of fsdet"
    This reverts commit 83abb1529dcd6203b8d85639c1d7ffcc6d1d6d7c.

--- fsdet/layers/csrc/ROIAlign/ROIAlign_cpu.cpp
+++ fsdet/layers/csrc/ROIAlign/ROIAlign_cpu.cpp
@@ -426,7 +426,7 @@ at::Tensor ROIAlign_forward_cpu(
   if (output.numel() == 0)
     return output;
 
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.options(), "ROIAlign_forward", [&] {
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(input.type(), "ROIAlign_forward", [&] {
     ROIAlignForward<scalar_t>(
         output_size,
         input.contiguous().data_ptr<scalar_t>(),
@@ -478,7 +478,7 @@ at::Tensor ROIAlign_backward_cpu(
   int h_stride = grad.stride(2);
   int w_stride = grad.stride(3);
 
-  AT_DISPATCH_FLOATING_TYPES_AND_HALF(grad.options(), "ROIAlign_forward", [&] {
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(grad.type(), "ROIAlign_forward", [&] {
     ROIAlignBackward<scalar_t>(
         grad.numel(),
         grad.contiguous().data_ptr<scalar_t>(),
--- fsdet/layers/csrc/nms_rotated/nms_rotated_cpu.cpp
+++ fsdet/layers/csrc/nms_rotated/nms_rotated_cpu.cpp
@@ -13,10 +13,10 @@ at::Tensor nms_rotated_cpu_kernel(
   // however, the code in this function is much shorter because
   // we delegate the IoU computation for rotated boxes to
   // the single_box_iou_rotated function in box_iou_rotated_utils.h
-  AT_ASSERTM(!dets.options().is_cuda(), "dets must be a CPU tensor");
-  AT_ASSERTM(!scores.options().is_cuda(), "scores must be a CPU tensor");
+  AT_ASSERTM(!dets.type().is_cuda(), "dets must be a CPU tensor");
+  AT_ASSERTM(!scores.type().is_cuda(), "scores must be a CPU tensor");
   AT_ASSERTM(
-      dets.options() == scores.options(), "dets should have the same type as scores");
+      dets.type() == scores.type(), "dets should have the same type as scores");
 
   if (dets.numel() == 0) {
     return at::empty({0}, dets.options().dtype(at::kLong));
@@ -64,7 +64,7 @@ at::Tensor nms_rotated_cpu(
     const float iou_threshold) {
   auto result = at::empty({0}, dets.options());
 
-  AT_DISPATCH_FLOATING_TYPES(dets.options(), "nms_rotated", [&] {
+  AT_DISPATCH_FLOATING_TYPES(dets.type(), "nms_rotated", [&] {
     result = nms_rotated_cpu_kernel<scalar_t>(dets, scores, iou_threshold);
   });
   return result;
